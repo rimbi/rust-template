be9556a8f6bf3ca0999852457a42b5af4de297a6
Cem Eliguzel <cem.eliguzel@rolos.com>
2024-01-28 10:50:14 +0300

    Update cargo-generate installation instructions and change the setting of tracing level.

--- README.md
+++ README.md
@@ -7,16 +7,23 @@ A template for a tokio app, which has arguments parsing, config parsing, tracing
 ## Usage
 
 1. Install Rust and Cargo
-2. Install `cargo-generate` with 
+
+2. Install `cargo-generate` with
+
 ```bash
 cargo install cargo-generate
 ```
+
 3. Generate a new project with
+
 ```bash
 cargo generate --git git@github.com:rimbi/rust-template.git --name <project-name>
 ```
-4. Answer the questions 
+
+4. Answer the questions
+
 5. Build and run the project with
+
 ```bash
 cd <project-name>
 cargo run
--- tokio-template/src/main.rs
+++ tokio-template/src/main.rs
@@ -1,9 +1,12 @@
-use std::sync::Arc;
+use std::{str::FromStr, sync::Arc};
 
 use anyhow::Context;
 use config::Config;
 use opentelemetry_sdk::{propagation::TraceContextPropagator, runtime::Tokio};
 use structopt::StructOpt;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::prelude::*;
 
 mod config;
 
@@ -17,8 +20,6 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    init_tracing();
-
     let args = Args::from_args();
 
     let config = Config::from_file(&args.config)
@@ -26,14 +27,14 @@ async fn main() -> anyhow::Result<()> {
         .context("Failed to get the config")?;
     let config = Arc::new(config);
 
-    println!("Success ✅");
+    init_tracing(&config.trace_level);
+
+    info!("Success ✅");
 
     Ok(())
 }
 
-fn init_tracing() {
-    use tracing_subscriber::prelude::*;
-
+fn init_tracing(tracing_level: &str) {
     opentelemetry::global::set_text_map_propagator(TraceContextPropagator::default());
 
     let tracer = opentelemetry_jaeger::new_agent_pipeline()
@@ -45,8 +46,18 @@ fn init_tracing() {
 
     let fmt_layer = tracing_subscriber::fmt::layer();
 
+    // The filter layer can be set via
+    // - RUST_LOG env variable
+    // - config file
+    // - default value `info`
+    // such that the former overrides the latter.
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new(tracing_level))
+        .or_else(|_| EnvFilter::from_str("info"))
+        .expect("Creating the filter layer can not fail!");
+
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(filter_layer)
         .with(fmt_layer)
         .with(telemetry_layer)
         .init();
